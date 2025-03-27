539c59a334f60a32e313bbe14b0040a706072dd9
d3v_friends <d3v.friends@gmail.com>
2025-03-27 11:49:58 +0900

    update tag.sh

--- script/tag.sh
+++ script/tag.sh
@@ -8,7 +8,7 @@ echo "tag=$TAG, base_tag=$BASE_TAG, origin=$ORIGIN"
 
 git tag -d "$TAG"
 git push -d "$ORIGIN" "$TAG"
-git tag ""
+git tag "$TAG"
 git push --tags "$ORIGIN" "$TAG"
 
 echo "upload tag done!"
